77f00b70e52b0a0bda15f7a30ae5415c44913f43
Christian Medina <medinachristian1984@gmail.com>
2017-03-01 11:28:25 -0400

    Se modificó el gulpfile a fin de que las rutas de las carpertas de los archivos .pug y .css fuesen las correctas. También se creó la carpeta de la primer clase y se añadieron los estilos a distintos elementos html

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,24 @@
 var gulp = require('gulp'),
-  pug = require('gulp-pug'),
-  sass = require('gulp-sass'),
-  pref = require('gulp-autoprefixer'),
-  plum = require('gulp-plumber'),
-  brow = require('browser-sync').create(),
-  reload = brow.reload
+    pug = require('gulp-pug'),
+    plum = require('gulp-plumber'),
+    brow = require('browser-sync').create(),
+    reload = brow.reload,
+    strm = reload({stream: true});
+
+var src = './01/';
 
 gulp.task('pug', function () {
-  return gulp.src('./dev/views/*.pug')
+  return gulp.src(src + '*.pug')
     .pipe(plum())
     .pipe(pug({pretty: true}))
     .pipe(plum.stop())
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest(src));
 })
 
-gulp.task('styles', function () {
-  return gulp.src('./dev/scss/styles.scss')
-    .pipe(plum())
-    .pipe(sass({outputStyle: 'nested'}))
-    .pipe(pref({browsers: 'last 5 versions', cascade: true}))
-    .pipe(plum.stop())
-    .pipe(gulp.dest('./dist/css/'))
-    .pipe(reload({stream: true}))
-})
+gulp.task('default', ['pug'], function () {
+  brow.init({server: src});
 
-gulp.task('default', ['pug', 'styles'], function () {
-  brow.init({server: './dist/'})
-
-  gulp.watch('./dev/views/**/*.pug', ['pug'])
-  gulp.watch('./dev/scss/**/*.scss', ['styles'])
-  gulp.watch('./dist/*.html').on('change', reload)
-  gulp.watch('./dist/js/*.js').on('change', reload)
+  gulp.watch(src + '*.pug', ['pug']);
+  gulp.watch(src + '*.html').on('change', reload);
+  gulp.watch(src + '*.css').on('change', reload);
 })
